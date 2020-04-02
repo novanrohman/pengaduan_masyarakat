0bd2ea10c5aefc27420afacfbb16c5ee1cc8c5e5
novanrohman <novanrohman@gmail.com>
2020-04-02 16:50:05 +0700

    update database

--- pengaduan_masyarakat.sql
+++ pengaduan_masyarakat.sql
@@ -2,8 +2,8 @@
 -- version 4.1.12
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Mar 29, 2020 at 08:57 
+-- Host: 127.0.0.1
+-- Generation Time: Apr 02, 2020 at 11:48 AM
 -- Server version: 5.6.16
 -- PHP Version: 5.5.11
 
@@ -27,14 +27,23 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `masyarakat` (
-  `nik` char(16) NOT NULL,
-  `nama` varchar(35) NOT NULL,
+  `nik` int(16) NOT NULL,
+  `name` varchar(35) NOT NULL,
   `username` varchar(25) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `telp` varchar(13) NOT NULL,
+  `telp` varchar(14) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
   PRIMARY KEY (`nik`),
-  UNIQUE KEY `username` (`username`)
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `masyarakat`
+--
+
+INSERT INTO `masyarakat` (`nik`, `name`, `username`, `telp`, `email`, `password`) VALUES
+(2147483647, 'novan rohman nur', 'novan_rohman', '081276995583', 'novanrohman@gmail.com', '598920707a94a6969b1f5cbe4561607f');
 
 -- --------------------------------------------------------
 
@@ -43,14 +52,14 @@ CREATE TABLE IF NOT EXISTS `masyarakat` (
 --
 
 CREATE TABLE IF NOT EXISTS `pengaduan` (
-  `id_pengaduan` int(11) NOT NULL AUTO_INCREMENT,
+  `id_pengaduan` int(11) NOT NULL,
   `tgl_pengaduan` date NOT NULL,
   `nik` char(16) NOT NULL,
   `isi_laporan` text NOT NULL,
   `foto` varchar(255) NOT NULL,
   `status` enum('menunggu','proses','selesai') NOT NULL,
   PRIMARY KEY (`id_pengaduan`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -59,13 +68,14 @@ CREATE TABLE IF NOT EXISTS `pengaduan` (
 --
 
 CREATE TABLE IF NOT EXISTS `petugas` (
-  `id_petugas` int(11) NOT NULL,
   `nama_petugas` varchar(35) NOT NULL,
-  `username` varchar(25) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `usr_petugas` varchar(25) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `telp` varchar(13) NOT NULL,
   `level` enum('admin','petugas') NOT NULL,
-  PRIMARY KEY (`id_petugas`)
+  PRIMARY KEY (`usr_petugas`),
+  UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -75,13 +85,13 @@ CREATE TABLE IF NOT EXISTS `petugas` (
 --
 
 CREATE TABLE IF NOT EXISTS `tanggapan` (
-  `id_tanggapan` int(11) NOT NULL,
+  `id_tanggapan` int(11) NOT NULL AUTO_INCREMENT,
   `id_pengaduan` int(11) NOT NULL,
   `tgl_tanggapan` date NOT NULL,
   `tanggapan` text NOT NULL,
-  `id_petugas` int(11) NOT NULL,
+  `usr_petugas` varchar(50) NOT NULL,
   PRIMARY KEY (`id_tanggapan`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
